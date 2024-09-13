9b4f9d027c26888d6aa9a98a6b810d9f199a6cfb
nurad <nuradhussen082@gmail.com>
2024-09-13 11:11:14 +0300

    static file url

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from bot1.views import *
 
 urlpatterns = [
@@ -7,4 +9,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("bot1/", webhook),
     path("bot2/", webhook2),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
